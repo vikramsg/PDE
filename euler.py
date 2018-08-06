64aebd53edba121ce7b7b38033a449b3fa82ee16
VSingh <vikramsingh001@gmail.com>
2018-08-06 16:15:50 +0300

    Adding Compressible Euler equations

--- euler.py
+++ euler.py
@@ -5,9 +5,20 @@ import matplotlib.pyplot as plt
 
 class EulerDG:
 
-    def __init__(self, order, elements, startX, stopX, correcType = 0, correcFac = 0.9):
+    def __init__(self, order, elements, startX, stopX):
+        ###########
+        # Constants
+        ##########
+
+        self.gamm     = 1.4
+        
+        ##########
+
+
         self.order    = order
         self.Np       = order + 1
+
+        self.var_dim  = 3
 
         self.elements = elements 
         self.startX   = startX 
@@ -15,60 +26,21 @@ class EulerDG:
 
         self.x_r, self.intPoints = self.dgMesh(self.elements, self.startX, self.stopX, self.order)
 
-        self.u   = np.zeros((self.elements, self.Np))
-        self.rhs = np.zeros((self.elements, self.Np))
+        self.u   = np.zeros((self.var_dim, self.elements, self.Np))
+        self.rhs = np.zeros((self.var_dim, self.elements, self.Np))
         
         self.project(self.fn, self.intPoints, self.u) # Set initial condition
 
         self.nodes = np.polynomial.legendre.leggauss(self.Np)[0] 
 
-        if correcType == 0:
-            self.dg_l  = Poly().leftRadauDeri(order, self.nodes)
-            self.dg_r  = Poly().rightRadauDeri(order, self.nodes)
-        elif correcType == 1:
-            self.dg_l  = Poly().getKrivodLeft(order, self.nodes, correcFac)
-            self.dg_r  = Poly().getKrivodRight(order, self.nodes, correcFac)
-        elif correcType == 2:
-            self.dg_l  = Poly().getFilterLeft(order, self.nodes)
-            self.dg_r  = Poly().getFilterRight(order, self.nodes)
-        else:
-            raise Exception('CorrecType not supported')
+        self.dg_l  = Poly().leftRadauDeri(order, self.nodes)
+        self.dg_r  = Poly().rightRadauDeri(order, self.nodes)
 
         gaussNodes = np.polynomial.legendre.leggauss(self.Np)[0] 
         self.dPhi  = Poly().lagrangeDeri(gaussNodes)
 
         self.l_R   = Poly().lagrange_right(order)
         self.l_L   = Poly().lagrange_left (order)
-
-        self.krivod_l_R = Poly().krivodInterpolate(self.nodes, order, correcFac,  1.0) 
-        self.krivod_l_L = Poly().krivodInterpolate(self.nodes, order, correcFac, -1.0) 
-
-        van   = Poly().vandermonde(order, self.nodes) 
-        vanF  = Poly().filterVandermonde(order, self.nodes)
-
-        self.filterVan  = np.dot(vanF, np.linalg.inv(van)) 
-
-
-
-    def plot(self, x, u):
-        elements = x.shape[0]
-        Np       = x.shape[1]
-
-        size     = elements*Np
-
-        xv       = np.zeros((size))
-        uv       = np.zeros((size))
-
-        coun = 0
-        for i in range(elements):
-            for j in range(Np):
-                xv[coun] = x[i, j]
-                uv[coun] = u[i, j]
-                coun     = coun + 1
-
-        plt.plot(xv, uv)
-
-        plt.show()
 
 
     def dgMesh(self, elements, startX, stopX, order):
@@ -103,77 +75,87 @@ class EulerDG:
 
         for i in range(elements):
             for j in range(Np):
-                u[i, j] = fn(x[i, j])
+                u[:, i, j] = fn(x[i, j])
+
+    def fn(self, x):
+        u    = np.zeros(3)
+
+        rho  = 1.0 + 0.2*np.sin(np.pi*(x))
+        p    = 1
+        vel  = 1
+
+        u[0] = rho
+        u[1] = rho*vel
+        u[2] = p/(self.gamm - 1) + 0.5*rho*vel*vel
+
+        return u 
 
 
     def getDiscontDeri(self, u):
-        elements = u.shape[0]
-        Np       = u.shape[1]
+        var_dim  = u.shape[0] #variable dimension of equation (=3 for Euler equations)
+        elements = u.shape[1] #Num of elements
+        Np       = u.shape[2] #Num of soln points
 
         du       = np.zeros_like(u)
 
         dPhi     = self.dPhi 
 
-        for i in range(elements):
-            du[i] = np.dot(np.array(dPhi), np.array(u[i]))
+        for i in range(var_dim):
+            for j in range(elements):
+                du[i, j, :] = np.dot(np.array(dPhi), np.array(u[i, j, :]))
 
         return(du)
 
+   
     def getDiscontBnd(self, u):
-        elements = u.shape[0]
-        Np       = u.shape[1]
-        order    = Np - 1
+        var_dim  = u.shape[0] #variable dimension of equation (=3 for Euler equations)
+        elements = u.shape[1] #Num of elements
+        Np       = u.shape[2] #Num of soln points
+      
+        order    = Np - 1 # Only true in 1D
 
         l_R      = self.l_R 
         l_L      = self.l_L 
 
-        u_d = np.zeros((elements, 2))
+        u_d = np.zeros((var_dim, elements, 2))
 
-        for i in range(elements):
-            u_d[i, 0] = np.dot(l_L, u[i])
-            u_d[i, 1] = np.dot(l_R, u[i])
+        for i in range(var_dim):
+            for j in range(elements):
+                u_d[i, j, 0] = np.dot(l_L, u[i, j, :])
+                u_d[i, j, 1] = np.dot(l_R, u[i, j, :])
 
         return(u_d)
 
 
-    def getInteractionValues(self, u_d):
-        elements = u_d.shape[0]
-        Np       = u_d.shape[1]
+    def getInteractionValues(self, u_d, f_d):
+        var_dim  = u_d.shape[0] #variable dimension of equation (=3 for Euler equations)
+        elements = u_d.shape[1] #Num of elements
 
-        u_I = np.zeros((elements, 2))
+        u_I = np.zeros((var_dim, elements, 2))
 
-        alpha = 0.5
-
-        #Central flux
-#        for i in range(1, elements - 1):
-#            u_I[i, 0] = alpha*(u_d[i - 1, 1] + u_d[i    , 0] )
-#            u_I[i, 1] = alpha*(u_d[i    , 1] + u_d[i + 1, 0] )
-#
-#        # Periodic boundary
-#        u_I[0       , 0] = alpha*(u_d[elements - 1, 1] + u_d[0    , 0] )
-#        u_I[0       , 1] = alpha*(u_d[0           , 1] + u_d[0 + 1, 0] )
-#
-#        u_I[elements - 1, 0] = alpha*(u_d[elements - 2, 1] + u_d[elements - 1, 0] )
-#        u_I[elements - 1, 1] = alpha*(u_d[elements - 1, 1] + u_d[0           , 0] )
-#
-        for i in range(1, elements - 1):
-            u_I[i, 0] = u_d[i - 1, 1] 
-            u_I[i, 1] = u_d[i    , 1] 
+        # Pure upwinding
+        for i in range(var_dim):
+            for j in range(1, elements - 1):
+                u_I[i, j, 0] = u_d[i , j - 1, 1] 
+                u_I[i, j, 1] = u_d[i , j    , 1] 
 
         # Periodic boundary
-        u_I[0       , 0] = u_d[elements - 1, 1] 
-        u_I[0       , 1] = u_d[0           , 1] 
+        for i in range(var_dim):
+            u_I[i,   0  , 0] = u_d[i, elements - 1, 1] 
+            u_I[i,   0  , 1] = u_d[i, 0           , 1] 
 
-        u_I[elements - 1, 0] = u_d[elements - 2, 1] 
-        u_I[elements - 1, 1] = u_d[elements - 1, 1] 
-
+            u_I[i, elements - 1, 0] = u_d[i, elements - 2, 1] 
+            u_I[i, elements - 1, 1] = u_d[i, elements - 1, 1] 
 
         return u_I
+
+
 
     def dg(self, u, dt):
         order     = self.order
         Np        = self.Np
         elements  = self.elements
+        var_dim   = self.var_dim
 
         intPoints = self.intPoints
         x_r       = self.x_r
@@ -181,20 +163,24 @@ class EulerDG:
         dg_l      = self.dg_l
         dg_r      = self.dg_r
 
-        du        = self.getDiscontDeri(u)        # Get uncorrected derivative
-        
-        u_d       = self.getDiscontBnd(u )         # Get discontinuous values at element boundary
+        f_E       = self.getEulerFlux(u)         # Get Euler flux 
 
-        u_I       = self.getInteractionValues(u_d)
+        df        = self.getDiscontDeri(f_E)     # Get uncorrected derivative
+        
+        u_edge    = self.getDiscontBnd(u   )     # Get discontinuous values at element boundary
+        f_edge    = self.getDiscontBnd(f_E )     # Get discontinuous values at element boundary
+
+        f_I       = self.getInteractionValues(u_edge, f_edge)
         
         rhs       = np.zeros_like(u)
 
-        for i in range(elements):
-            rhs[i] = du[i]  
-            rhs[i] = rhs[i] + (u_I[i, 0] - u_d[i, 0])*dg_l 
-            rhs[i] = rhs[i] + (u_I[i, 1] - u_d[i, 1])*dg_r 
-        
-            rhs[i] = -1*rhs[i]/x_r[i]
+        for i in range(var_dim):
+            for j in range(elements):
+                rhs[i, j] = df[i, j]  
+                rhs[i, j] = rhs[i, j] + (f_I[i, j, 0] - f_edge[i, j, 0])*dg_l 
+                rhs[i, j] = rhs[i, j] + (f_I[i, j, 1] - f_edge[i, j, 1])*dg_r 
+            
+                rhs[i, j] = -1*rhs[i, j]/x_r[j]
 
         return rhs
 
@@ -234,89 +220,61 @@ class EulerDG:
         return u 
 
 
-    def wave_solver(self, dt, T_final):
-        u   = self.u
-
-        T   = 0
-        dt_real = min(dt, T_final - T)
-
-        it_coun = 0
-        while (T < T_final) :
-            u   = self.ssp_rk33(dt, u, self.dg) 
-
-            T       = T + dt_real
-            dt_real = min(dt, T_final - T)
-
-            if (it_coun % 10 == 0):
-                print('Time: ', T, ' Max u: ', np.max(u))
-
-            it_coun  = it_coun + 1
-
-        self.u = u
         
-        self.plot(self.intPoints, u)
+    def getEulerFlux(self, u):
+        '''
+        Get the 1D euler fluxes for the given solution vector
+        '''
+        var_dim  = u.shape[0] #variable dimension of equation (=3 for Euler equations)
+        elements = u.shape[1] #Num of elements
+        Np       = u.shape[2] #Num of soln points
 
-    def filter_van(self, u):
-        order    = self.order
-        Np       = self.Np
-
-        elements = self.elements 
-
-        filter_v = self.filterVan
-
+        f        = np.zeros_like(u)
+      
         for i in range(elements):
-            u[i] = np.dot(filter_v, u[i])
+            for j in range(Np):
+                rho   = u[0, i, j]
+                rhoV  = u[1, i, j]
+                rho_e = u[2, i, j]
 
+                v      = rhoV/rho
+                v_sq   = v*v
 
-    def filtered_wave_solver(self, dt, T_final):
-        u   = self.u
-        self.filter_van(u)
+                p      = (self.gamm - 1)*(rho_e - 0.5*rho*v_sq)
 
-        T   = 0
-        dt_real = min(dt, T_final - T)
+                f[0, i, j] = rhoV
+                f[1, i, j] = rho*v_sq + p
+                f[2, i, j] = (rho_e + p)*v 
 
-        it_coun = 0
-        while (T < T_final) :
-            u       = self.ssp_rk33(dt, u, self.dg) 
-            self.filter_van(u)
-
-            T       = T + dt_real
-            dt_real = min(dt, T_final - T)
-
-            if (it_coun % 10 == 0):
-                print('Time: ', T, ' Max u: ', np.max(u))
-
-            it_coun  = it_coun + 1
-
-        self.u = u
-        
-        self.plot(self.intPoints, u)
+        return f
 
     
-    def error(self):
-        order     = self.order
-        Np        = self.Np
 
-        elements  = self.elements 
+    def euler_solver(self, dt, T_final):
+        u   = self.u
 
-        intPoints = self.intPoints
+        T   = 0
+        dt_real = min(dt, T_final - T)
 
-        ex        = np.zeros((self.elements, self.Np))
+        it_coun = 0
+        while (T < T_final) :
+            u   = self.euler(dt, u, self.dg) 
 
-        self.project(self.fn, intPoints, ex) # Set exact condition
+            T       = T + dt_real
+            dt_real = min(dt, T_final - T)
 
-        l2_error  = np.linalg.norm(ex - self.u) 
+            if (it_coun % 10 == 0):
+                print('Time: ', T, ' Max u: ', np.max(u))
 
-        print("L2 error is ", l2_error/(Np*elements))
+            it_coun  = it_coun + 1
 
-    def fn(self, x):
-#        return 1.0 + 0.2*np.sin(np.pi*(x))
-        return np.exp(-40*(x)**2) 
-
+#        self.u = u
+        
+ 
 
 if __name__=="__main__":
-    order    = 5
-    elements = 25
+    order    = 3
+    elements = 10
     startX   = -1
     stopX    =  1
     
@@ -325,11 +283,11 @@ if __name__=="__main__":
     0: Radau
     1: Krivodnova , takes correcFac
     '''
-    run     = EulerDG(order, elements, startX, stopX, correcType = 1, correcFac = 1.0)
+    run     = EulerDG(order, elements, startX, stopX)
 
-    dt      = 0.003  
+    dt      = 0.0001 
 
     T_final = 8.0
-#    run.wave_solver(dt, T_final)
+    run.euler_solver(dt, T_final)
 
 
